8cd559558857f3b75c69d66ba18368c33ad2dec4
kym9129 <kym9129@naver.com>
2023-10-29 20:25:58 +0900

    chore: todo 주석 추가

--- src/test/java/com/example/designpattern/StarbuzzCoffeeTest.java
+++ src/test/java/com/example/designpattern/StarbuzzCoffeeTest.java
@@ -18,7 +18,7 @@ public class StarbuzzCoffeeTest {
         beverage2 = new Mocha(beverage2);
         beverage2 = new Mocha(beverage2); // 모캇 2샷. (감싸기)
         beverage2 = new Milk(beverage2); // 우유 추가 (감싸기)
-        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost()); // todo: 가격 $1.3900000000000001로 나오는 이슈 디버깅
     }
 
 }
